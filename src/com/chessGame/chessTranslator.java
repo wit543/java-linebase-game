8415a54fcaee6b9d519de85efe156a30bc3f418b
wit543 <norawit543@hotmail.com>
2013-11-04 14:59:08 +0700

    Update chessTranslator.java

--- src/com/chessGame/chessTranslator.java
+++ src/com/chessGame/chessTranslator.java
@@ -1,5 +1,9 @@
 package com.chessGame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class chessTranslator {
 	public static int IntfromX;
 	public static int IntfromY;
@@ -21,21 +25,46 @@ public class chessTranslator {
 		System.out.println(fromY);
 		System.out.println(toX);
 		System.out.println(toY);
-		IntfromY = 0;
+                IntfromY = 0;
 		InttoY = 0;
-		String[] out = {"a","b","c","d","e","f","g","h"};
-		while(IntfromY <8){
-			IntfromY++;
-			if(fromY.equals(out[IntfromY])){
-				IntfromY++;
-				break;
-			}
+
+                String[] out = {"a","b","c","d","e","f","g","h"};
+                /*for(int i=0;i<8;i++){
+                    if("a".equals(fromY)){
+                       IntfromY = 1;
+                    }if("b".equals(fromY)){
+                       IntfromY = 2;
+                    }if("c".equals(fromY)){
+                       IntfromY = 3;
+                    }if("d".equals(fromY)){
+                       IntfromY = 4;
+                    }if("e".equals(fromY)){
+                       IntfromY = 5;
+                    }if("f".equals(fromY)){
+                       IntfromY = 6;
+                    }if("g".equals(fromY)){
+                       IntfromY = 7;
+                    }if("h".equals(fromY)){
+                       IntfromY = 8;
+                    }
+                }
+                */
+		//List<String> out = Arrays.asList("a","b","c","d","e","f","g","h");
+                //1/a-2/b
+                System.out.println(fromY);
+		while(IntfromY <9){
+                    System.out.println(IntfromY);
+                    IntfromY++;
+                    if(fromY.equals(out[chessTranslator.IntfromY].toString())){
+                        IntfromY++;
+                        break;
+                    }
 		}
-		while(InttoY <8){
+                while(InttoY <9){
 			InttoY++;
-			if(toY.equals(out[InttoY])){
-				InttoY++;
-				break;
+			if(toY.equals(out[chessTranslator.InttoY].toString())){
+                            InttoY++;
+                            break;
 			}
 		}
 		IntfromX = Integer.parseInt(fromX);
